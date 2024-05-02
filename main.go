1e30707199600b407704725a9cc4d70c258124e0
Andrej Ceremnih <andrejceremnih@gmail.com>
2024-05-02 09:49:06 +0300

    main.go

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"image"
+	"image/color"
 	"log"
 	"math/rand"
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
@@ -45,17 +49,58 @@ func main() {
 
 	)
 
-	var (
-		inputs, labels []float64
-		xys            plotter.XYs
-	)
-	f := func(x float64) float64 { return 17*x - 1.75 }
-	for i := 0; i < inputPoints; i++ {
-		inputs = append(inputs, inputPointsMinX+(inputPointsMaxX-inputPointsMinX)*rand.Float64())
-		labels = append(labels, f(inputs[i])+inputPointsRandY*(1-rand.Float64()*2))
-		xys = append(xys, plotter.XY{X: inputs[i], Y: labels[i]})
+	houses, err := readFromCSV("data/house_prices.csv")
+	if err != nil {
+		log.Fatalf("Can't read Houses from CSV: %v", err)
 	}
-	inputsScatter, _ := plotter.NewScatter(xys)
+	var inputs, labels []float64
+	types, colors := make([][]float64, len(houses)), make([][]float64, len(houses))
+	typeId := map[string][]float64{
+		"Duplex":        []float64{1, 0, 0, 0, 0},
+		"Detached":      []float64{0, 1, 0, 0, 0},
+		"Semi-detached": []float64{0, 0, 1, 0, 0},
+		"Townhouse":     []float64{0, 0, 0, 1, 0},
+		"Multi-family":  []float64{0, 0, 0, 0, 1},
+	}
+	colorId := map[string]int{
+		"brown":  0,
+		"yellow": 1,
+		"white":  2,
+		"blue":   3,
+		"green":  4,
+	}
+	for i, house := range houses {
+		labels = append(labels, house.Price)
+		inputs = append(inputs, house.Square)
+
+		colorIndex := colorId[house.WallColor]
+		colors[i] = make([]float64, 5)
+		colors[i][colorIndex] = 1
+	}
+
+	xys := make([]plotter.XYs, 5)
+	for i := 0; i < len(inputs); i++ {
+		for j := 0; j < 5; j++ {
+			if types[i][j] == 1 {
+				xys[j] = append(xys[j], plotter.XY{X: inputs[i], Y: labels[i]})
+			}
+		}
+	}
+	var inputsScatter []*plotter.Scatter
+	color := []color.RGBA{
+		{0, 0, 0, 255},
+		{255, 0, 0, 255},
+		{0, 255, 0, 255},
+		{0, 0, 255, 255},
+		{255, 0, 255, 255},
+		{0, 255, 255, 255},
+	}
+
+	for i := 0; i < 5; i++ {
+		tmp, _ := plotter.NewScatter(xys[i])
+		inputsScatter = append(inputsScatter, tmp)
+		inputsScatter[i].Color = color[i]
+	}
 
 	img := make(chan *image.RGBA, 1) // Have at most one image in the channel.
 	render := func(x *image.RGBA) {
@@ -66,11 +111,13 @@ func main() {
 		}
 	}
 	go func() {
-		w := startValueRange - rand.Float64()*2*startValueRange
-		b := startValueRange - rand.Float64()*2*startValueRange
+		w := make([]float64, 12)
+		for i := range w {
+			w[i] = startValueRange - rand.Float64()*2*startValueRange
+		}
 		var loss plotter.XYs
 		for i := 0; i < epochs; i++ {
-			y := inference(inputs, w, b)
+			y := inference(inputs, w, types)
 			loss = append(loss, plotter.XY{
 				X: float64(i),
 				Y: msl(labels, y),
@@ -81,23 +128,23 @@ func main() {
 			} else {
 				const extra = (inputPointsMaxX - inputPointsMinX) / 10
 				xs := []float64{inputPointsMinX - extra, inputPointsMaxX + extra}
-				ys := inference(xs, w, b)
+				ys := inference(xs, w, types)
 				resLine, _ := plotter.NewLine(plotter.XYs{{X: xs[0], Y: ys[0]}, {X: xs[1], Y: ys[1]}})
 				render(Plot(inputsScatter, resLine))
 			}
 			dw, db := dmsl(inputs, labels, y)
-			w += dw * learningRateW
-			b += db * learningRateB
+			// w += dw * learningRateW
+			// b += db * learningRateB
 			//time.Sleep(30 * time.Millisecond)
 			if i%printEveryNthEpochs == 0 {
 				fmt.Printf(`Epoch #%d
 	loss: %.4f
 	dw: %.4f, db: %.4f
 	w : %.4f,  b: %.4f
-`, i, loss[len(loss)-1].Y, dw, db, w, b)
+`, i, loss[len(loss)-1].Y, dw, db, w)
 			}
 		}
-		fmt.Println(w, b)
+		fmt.Println(w)
 	}()
 
 	if err := ebiten.RunGame(&App{Img: img}); err != nil {
@@ -105,9 +152,9 @@ func main() {
 	}
 }
 
-func inference(inputs []float64, w, b float64) (res []float64) {
-	for _, x := range inputs {
-		res = append(res, w*x+b)
+func inference(inputs, w []float64, t [][]float64) (res []float64) {
+	for i, x := range inputs {
+		res = append(res, (w[0]*t[i][0]+w[1]*t[i][1]+w[2]*t[i][2]+w[3]*t[i][3]+w[4]*t[i][4]+w[5])+(w[6]*t[i][0]+w[7]*t[i][1]+w[8]*t[i][2]+w[9]*t[i][3]+w[10]*t[i][4]+w[11])*x)
 	}
 	return res
 }
@@ -126,4 +173,51 @@ func dmsl(inputs, labels, y []float64) (dw, db float64) {
 		db += diff
 	}
 	return 2 * dw / float64(len(labels)), 2 * db / float64(len(labels))
+}
+
+type House struct {
+	Square    float64
+	Type      string
+	Price     float64
+	WallColor string
+}
+
+func readFromCSV(filename string) ([]House, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	reader := csv.NewReader(file)
+	reader.FieldsPerRecord = 4
+	data, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var houses []House
+	for i, record := range data {
+		if i == 0 {
+			continue
+		}
+		square, err := strconv.ParseFloat(record[0], 64)
+		if err != nil {
+			log.Fatalf("Invalid square: %v", record)
+			continue
+		}
+
+		price, err := strconv.ParseFloat(record[2], 64)
+		if err != nil {
+			log.Fatalf("Invalid price: %v", record)
+			continue
+		}
+
+		houses = append(houses, House{
+			Square:    square,
+			Type:      record[1],
+			Price:     price,
+			WallColor: record[3],
+		})
+	}
+	return houses, nil
 }
